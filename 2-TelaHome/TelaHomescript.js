3ce6db05029f8f8a6d35312034090f83b81cb392
Rafael Barbosa Ramos <141770380+RafaelADS7@users.noreply.github.com>
2025-04-24 01:00:29 -0300

    Adicionando a funcionaldiade de mudancas de cores nos botoes

--- 2-TelaHome/TelaHomescript.js
+++ 2-TelaHome/TelaHomescript.js
@@ -12,3 +12,51 @@ menuIcon.addEventListener('click', function() {
     }
 
 });
+
+const myButton1 = document.getElementById('myButton1');
+const myButton2 = document.getElementById('myButton2');
+const myButton3 = document.getElementById('myButton3');
+// Adiciona evento de clique para o botão 1
+myButton1.addEventListener('click', function () {
+    if (myButton1.style.backgroundColor === 'rgb(217, 217, 217)') {
+        myButton1.style.backgroundColor = '#1976D2';
+        myButton1.style.color = 'white';
+        myButton2.style.backgroundColor = '#D9D9D9';
+        myButton3.style.backgroundColor = '#D9D9D9';
+    } else {
+        myButton1.style.backgroundColor = '#D9D9D9';
+        myButton2.style.backgroundColor = '#D9D9D9';
+        myButton3.style.backgroundColor = '#D9D9D9';
+        myButton1.style.color = 'black';
+    }
+});
+
+// Adiciona evento de clique para o botão 2
+myButton2.addEventListener('click', function () {
+    if (myButton2.style.backgroundColor === 'rgb(217, 217, 217)') {
+        myButton1.style.backgroundColor = '#D9D9D9';
+        myButton2.style.backgroundColor = '#1976D2';
+        myButton2.style.color = 'white';
+        myButton3.style.backgroundColor = '#D9D9D9';
+    } else {
+        myButton1.style.backgroundColor = '#D9D9D9';
+        myButton2.style.backgroundColor = '#D9D9D9';
+        myButton3.style.backgroundColor = '#D9D9D9';
+        myButton2.style.color = 'black';
+    }
+});
+
+// Adiciona evento de clique para o botão 3
+myButton3.addEventListener('click', function () {
+    if (myButton3.style.backgroundColor === 'rgb(217, 217, 217)') {
+        myButton1.style.backgroundColor = '#D9D9D9';
+        myButton2.style.backgroundColor = '#D9D9D9';
+        myButton3.style.backgroundColor = '#1976D2';
+        myButton3.style.color = 'white';
+    } else {
+        myButton1.style.backgroundColor = '#D9D9D9';
+        myButton2.style.backgroundColor = '#D9D9D9';
+        myButton3.style.backgroundColor = '#D9D9D9';
+        myButton3.style.color = 'black';
+    }
+});
